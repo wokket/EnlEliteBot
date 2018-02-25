d91ace995cd8bfe119074d2307a3fda93d47553a
wokket <wokket@gmail.com>
2018-02-26 09:23:22 +1000

    fix: Update to new EBGS API Broke the System Lookup.

--- EnlEliteBot.Web/EDDB/EDDBSystemInfo.cs
+++ EnlEliteBot.Web/EDDB/EDDBSystemInfo.cs
@@ -16,12 +16,12 @@ namespace EnlEliteBot.Web.EDDB
     public class EDDBSystemInfo : ICoordinates
     {
 
-        public string Url { get { return $"https://eddb.io/system/{id}"; } }
+        public string Url { get { return $"https://eddb.io/system/{eddb_id}"; } }
 
         public string _id { get; set; }
         public int id { get; set; }
         public string name_lower { get; set; }
-        public int edsm_id { get; set; }
+        public int eddb_id { get; set; }
         public string name { get; set; }
 
         public float y { get; set; }
